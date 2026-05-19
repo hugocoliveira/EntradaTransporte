0cf97d8b1359f097e24d501b53483bb3540740ea
Hugo <chagas.hugo@gmail.com>
2026-05-18 22:28:50 -0300

    seguranca: bloqueia abertura direta sem extra 'origem'

--- app/src/main/java/com/entrada/transporte/MainActivity.kt
+++ app/src/main/java/com/entrada/transporte/MainActivity.kt
@@ -28,6 +28,13 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Bloqueia abertura direta: só aceita lançamentos vindos do MenuAutomatico
+        if (intent.getStringExtra("origem") != PACKAGE_APP_CHAMADOR) {
+            finishAndRemoveTask()
+            return
+        }
+
         enableEdgeToEdge()
 
         onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
