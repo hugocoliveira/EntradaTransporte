0f981364441478f5c2342ac84b165a589244728a
Hugo <chagas.hugo@gmail.com>
2026-05-26 11:54:06 -0300

    release: v1.3 (versionCode 3)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.entrada.transporte"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
